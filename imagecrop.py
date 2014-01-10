2bb7b9d7e4bfffb13706eea1714bc062f904aec3
Phaneendra Hegde <phaneendra.hegde@gmail.com>
2014-01-10 16:56:44 +0530

    Description added

--- imagecrop.py
+++ imagecrop.py
@@ -13,19 +13,21 @@ im = Image.open("img1.jpg")
 #im = trim(im)
 im = im.convert("RGBA")
 
-pixels = im.load()
+pixels = im.load() 
+# Loop from left to right for better replacement of pixels. 
 for i in range(im.size[1]):    # for every pixel from left to right
     for j in range(im.size[0]):
-    	if pixels[j,i] <= (255,255,255,255) and pixels[j,i] >= (245,245,245,245) : 
-    		pixels[j,i] = (0,0,0,0)
+    	if pixels[j,i] <= (255,255,255,255) and pixels[j,i] >= (245,245,245,245) :  # Check for grey shade in white. 
+    		pixels[j,i] = (0,0,0,0) # Put any backgroud color which matches your site theme.
     	else:
     		break
 for i in reversed(range(im.size[1])):    # for every pixel from right to left
     for j in reversed(range(im.size[0])):
     	if pixels[j,i] <= (255,255,255,255) and pixels[j,i] >= (245,245,245,245) : 
-    		pixels[j,i] = (0,0,0,0)
+    		pixels[j,i] = (0,0,0,0) 
     	else:
     		break
+    		
 # im.save("img2.png","PNG") # For storing transperant image in png format 
 im.save("img2.jpg", "JPEG")
 
